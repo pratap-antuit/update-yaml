11d45f1994b2be6aa824783aaf9274b4f16f75fe
Pratap Patil <pratap.patil@zebra.com>
2023-06-09 13:39:02 +0530

    v2 release

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ function loopThroughObjRecurs(obj, parseObject) {
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const inputs = core.getInput('milliseconds');
+    const inputs = core.getInput('inputs');
     core.info(`action input ${inputs} ...`);
     for (const k in inputs) {
       const yaml_data = yaml.load(fs.readFileSync(k, 'utf8'))
